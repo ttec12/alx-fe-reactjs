0685ae435171494c8c6a4573aaf9f421844027f8
Theophilus Turkson <80103060+ttec12@users.noreply.github.com>
2025-07-12 09:51:57 +0000

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
