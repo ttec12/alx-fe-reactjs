624d1af131e8952a6913ed507cc897afb8a550a0
Theophilus Turkson <80103060+ttec12@users.noreply.github.com>
2025-08-02 10:58:45 +0000

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Search from './components/search'
+import Search from './components/Search'
 import './index.css'; // Or './App.css' if you used that
 
 function App() {
