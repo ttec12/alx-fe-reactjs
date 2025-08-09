702fd71e8d00d5d663f718948fe56c04dc7d2326
Theophilus Turkson <80103060+ttec12@users.noreply.github.com>
2025-08-09 10:58:25 +0000

    Add files via upload

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
