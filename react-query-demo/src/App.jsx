2dfab2437534c6e0cbcf86260ae9925178af9138
Theophilus Turkson <80103060+ttec12@users.noreply.github.com>
2025-08-23 14:17:01 +0000

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,14 @@
-// src/App.jsx
-import PostsComponent from "./components/PostsComponent";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function App() {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <PostsComponent />
-    </div>
-  );
-}
+const queryClient = new QueryClient();
 
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
