b651e17a31d07b52b0c728f11f9cd2bc14ec0bae
Theophilus Turkson <80103060+ttec12@users.noreply.github.com>
2025-08-23 14:14:25 +0000

    Update formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,2 +1,8 @@
 import { Formik, Form, Field, ErrorMessage, initialValues,validationSchema } from "formik";
+import * as Yup from "yup";
+const schema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  ...
+});
+
 
